8e640575f4df6adc0f7d181aa1d6e7f51a966576
SGoodman <imhkentharita@gmail.com>
2026-03-03 17:45:57 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:45:54.045Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-03T14:45:56.610Z
+Commit: Patch API endpoint error handling
