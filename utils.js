cf90d6150a257c6f64f4b082e9c4b207b04456e4
SGoodman <imhkentharita@gmail.com>
2026-03-03 17:45:20 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:45:18.429Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-03T14:45:20.428Z
+Commit: Fix memory leak in event handlers
