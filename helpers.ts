a74e079dfd87aba70b4bb17c40b16ac203f437b6
SGoodman <imhkentharita@gmail.com>
2026-03-03 17:45:30 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T16:57:51.764Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-03T14:45:29.407Z
+Commit: Patch API endpoint error handling
