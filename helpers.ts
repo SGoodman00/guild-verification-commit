22ccddac2fa04646a4853f1a754865a3f7324690
SGoodman <imhkentharita@gmail.com>
2026-03-03 17:46:06 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:45:56.610Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-03T14:46:05.469Z
+Commit: Fix memory leak in event handlers
