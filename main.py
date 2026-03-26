20489949e722227445904e1d5606afa1b3989332
SGoodman <imhkentharita@gmail.com>
2026-03-27 02:16:05 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T23:15:49.625Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-26T23:16:04.678Z
+Commit: Fix broken links in documentation
