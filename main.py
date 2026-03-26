78105c057a95c79bea79cf7ff14ba6132505c267
SGoodman <imhkentharita@gmail.com>
2026-03-27 02:15:05 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T23:15:01.832Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-26T23:15:04.719Z
+Commit: Fix memory leak in event handlers
