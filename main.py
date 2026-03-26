b9ec10d76da1740b19aedbf225ac4cc1d7b2564c
SGoodman <imhkentharita@gmail.com>
2026-03-27 02:15:43 +0300

    Fix database connection timeout

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T23:15:13.306Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-26T23:15:42.875Z
+Commit: Fix database connection timeout
